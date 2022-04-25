7b226041501f08c9983666aa327e5c44cb8f0296
Equinn Nightfury <xianzyip8@gmail.com>
2022-04-25 20:23:33 +0800

    Created coding env

---
+++ me-website-mark-ii/README.md
@@ -0,0 +1,8 @@
+# ME-Website-Mark-II
+## Enhancing, upgrading, self-learning my web development skills.
+
+Intergrating Tailwind, React.js, Three.js
+
+Maybe I am asking too much to myself.
+
+
---
+++ me-website-mark-ii/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        title: ['Allura'],
+        para: ['"Playfair Display"']
+      }
+    },
+  },
+  plugins: [],
+}
